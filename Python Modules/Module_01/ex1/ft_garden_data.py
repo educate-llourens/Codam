6f8a841aed54f6e8bfe75e8c56fcc0eacff5d806
Leandra Lourens <llourens.codam@gmail.com>
2025-12-10 17:38:07 +0100

    🚧 Added Module_01 and got up to ex3

---
+++ Python Modules/Module_01/ex1/ft_garden_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        """Contins the data and behaviour of a plant
+
+        Args:
+            name (str): Name of the plant
+            height (int): Height of the plant
+            age (int): Age of the plant
+        """
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def print_plant(self) -> None:
+        """Prints the plant data in the specified format
+
+        Args:
+            name (str): Name of the plant
+            height (int): Height of the plant
+            age (int): Age of the plant
+        """
+        print(f"{self.name.capitalize()}: {self.height} cm, "
+              f"{self.age} days old")
+
+
+rose: Plant = Plant("rose", 25, 30)
+sunflower: Plant = Plant("sunflower", 80, 45)
+cactus: Plant = Plant("cactus", 15, 120)
+
+print("=== Garden Plant Registry ===")
+rose.print_plant()
+sunflower.print_plant()
+cactus.print_plant()
