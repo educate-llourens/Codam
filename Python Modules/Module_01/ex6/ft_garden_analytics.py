abd89379f69f3f75785bb2a187878d450bad101a
Leandra Lourens <llourens.codam@gmail.com>
2025-12-15 15:38:03 +0100

    🚧 Ex06

--- Python Modules/Module_01/ex6/ft_garden_analytics.py
+++ Python Modules/Module_01/ex6/ft_garden_analytics.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env Python3
 
 class GardenManager:
+    def __init__(self, garden_name: str):
+        self.garden_name = garden_name
     
+    def create_garden_network(self) -> None:
+
+
+
+class Garden_Stats(GardenManager):
+    def __init__(self, garden_name: str):
+        super().__init__(garden_name)
+        
+
+class Plant(GardenManager):
+    def __init__(self, garden_name: str, plant_name: str,
+                 plant_height: int, plant_age: int):
+        super().__init__(garden_name)
+        self.plant_name = plant_name
+        self.plant_height = plant_height
+        self.plant_age = plant_age
+    
+    @classmethod
+    def grow(cls, plant_height) -> "Plant":
+        plant_height += 1
+        return cls(plant_height)
+        
+
+class FloweringPlant(Plant):
+    def __init__(self, garden_name: str, plant_name: str, plant_height: int,
+                 plant_age: int, flower_colour: str):
+        super().__init__(garden_name, plant_name, plant_height, plant_age)
+    
+    @classmethod
+    def bloom(cls) -> str:
+        return "Blooming"
+
+class PrizeFlower(FloweringPlant):
+    def __init__(self, garden_name, plant_name, plant_height, plant_age):
+        super().__init__(garden_name, plant_name, plant_height, plant_age)
+
+
+def grow_plants() -> None:
+    
+
+print("=== Garden Management System Demo ===\n")
+
