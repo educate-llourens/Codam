5eaab6abd90f21623119d9e1226f2a09d60edee7
Leandra Lourens <llourens.codam@gmail.com>
2025-12-19 16:20:18 +0100

    🚧 More progress on ex6

--- Python Modules/Module_01/ex6/ft_garden_analytics.py
+++ Python Modules/Module_01/ex6/ft_garden_analytics.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, plant_name: str, plant_height: int,
-                 plant_age: int) -> None:
+    def __init__(self, plant_name: str, plant_height: int) -> None:
         self.plant_name = plant_name
         self.plant_height = plant_height
-        self.plant_age = plant_age
 
     def grow(self) -> None:
         self.plant_height += 1
-    
-    def set_height(self) -> None:
-        
 
 
 class FloweringPlant(Plant):
-    def __init__(self, plant_name, plant_height, plant_age):
-        super().__init__(plant_name, plant_height, plant_age)
+    def __init__(self, plant_name, plant_height, colour: str):
+        self.colour = colour
+        super().__init__(plant_name, plant_height)
 
 
 class PrizeFlower(FloweringPlant):
-    def __init__(self, plant_name, plant_height, plant_age):
-        super().__init__(plant_name, plant_height, plant_age)
+    def __init__(self, plant_name, plant_height, colour: str,
+                 prize_points: int):
+        super().__init__(plant_name, plant_height, colour)
 
 
 class GardenManager:
@@ -30,18 +27,54 @@ class GardenManager:
         self.plants: list[Plant] = []
 
     @classmethod
-    def create_garden_network(cls,
-                              garden_names: list[str]
-                              ) -> list['GardenManager']:
-        gardens: list[GardenManager] = []
-        i: int = 0
-        while i < len(garden_names):
-            garden = cls(garden_names[i])
-            gardens.append(garden)
-            i += 1
-        return gardens
+    def create_garden_network(cls) -> 'GardenManager':
+        manager = cls('Alice')
+        manager.add_plant(Plant("Oak Tree", 100))
+        manager.add_plant(FloweringPlant("Rose", 25, "red"))
+        manager.add_plant(PrizeFlower("Sunflower", 50, "yellow", 10))
+        return manager
 
-    def add_plant(self, plant_name: str, plant_height: int,
-                  plant_age: int) -> None:
-        new_plant: Plant = Plant(plant_name, plant_height, plant_age)
-        self.plants.append(new_plant)
+    def add_plant(self, plant: Plant) -> None:
+        self.plant: Plant = plant
+        self.plants.append(plant)
+        print(f"Added {plant.plant_name} to {self.garden_name}'s garden")
+
+    class GardenStats:
+        def __init__(self, garden: 'GardenManager', growth: int):
+            self.garden: GardenManager = garden
+            self.nbr_plants: int = len(garden.plants)
+            self.growth: int = 0
+
+        @staticmethod
+        def print_stats(garden: 'GardenManager') -> None:
+            print(f"=== {garden.garden_name}'s Garden Report ===")
+
+    @staticmethod
+    def growth_valid(days: int) -> bool:
+        if days > 0:
+            return True
+        return False
+
+    def grow(self, days: int) -> None:
+        if self.growth_valid(days):
+            print(f"{self.garden_name} is helping all the plants grow...")
+            i: int = 0
+            while i < len(self.plants):
+                self.plants[i].plant_height += days
+                print(f"{self.plants[i].plant_name} grew {days}cm")
+                i += 1
+            GardenManager.GardenStats(self, len(self.plants) * days)
+        else:
+            print("You entered an invalid growth parameter")
+
+
+def 
+
+if __name__ == "__main__":
+    print("=== Garden Management System Demo ===\n")
+    alice = GardenManager.create_garden_network()
+    print("")
+    alice.grow(1)
+    print("")
+    
+    alice.GardenStats.print_stats(alice)
