3607e8bfa24527de4c29ad46d68d2eeed8aee5d8
llourens <llourens@f0r3s7.codam.nl>
2026-01-05 21:45:12 +0100

    🚧 ex6: Added information to the Garden report:     - Plants have more information depending on the class     - Added stats for added plants and growth.

--- Python Modules/Module_01/ex6/ft_garden_analytics.py
+++ Python Modules/Module_01/ex6/ft_garden_analytics.py
@@ -18,6 +18,7 @@ class FloweringPlant(Plant):
 class PrizeFlower(FloweringPlant):
     def __init__(self, plant_name, plant_height, colour: str,
                  prize_points: int):
+        self.prize_points = prize_points
         super().__init__(plant_name, plant_height, colour)
 
 
@@ -25,6 +26,7 @@ class GardenManager:
     def __init__(self, garden_name: str) -> None:
         self.garden_name: str = garden_name
         self.plants: list[Plant] = []
+        self.list_len = len(self.plants)
 
     @classmethod
     def create_garden_network(cls) -> 'GardenManager':
@@ -37,17 +39,40 @@ class GardenManager:
     def add_plant(self, plant: Plant) -> None:
         self.plant: Plant = plant
         self.plants.append(plant)
+        self.list_len += 1
         print(f"Added {plant.plant_name} to {self.garden_name}'s garden")
 
     class GardenStats:
         def __init__(self, garden: 'GardenManager', growth: int):
             self.garden: GardenManager = garden
             self.nbr_plants: int = len(garden.plants)
-            self.growth: int = 0
+        growth: int = 0
+        plant_added: int = 0
 
         @staticmethod
         def print_stats(garden: 'GardenManager') -> None:
+            i: int = 0
             print(f"=== {garden.garden_name}'s Garden Report ===")
+            print("Plants in garden:")
+            while i < len(garden.plants):
+                plant = garden.plants[i]
+                if isinstance(plant, PrizeFlower):
+                    print(f"- {plant.plant_name}: "
+                          f"{plant.plant_height}cm, "
+                          f"{plant.colour} (blooming), "
+                          f"Prize points: {plant.prize_points}")
+                elif isinstance(plant, FloweringPlant):
+                    print(f"- {plant.plant_name}: "
+                          f"{plant.plant_height}cm, "
+                          f"{plant.colour} (blooming)")
+                elif isinstance(plant, Plant):
+                    print(f"- {plant.plant_name}: "
+                          f"{plant.plant_height}cm")
+                i += 1
+            print(
+                f"\nPlants added: "
+                f"{len(garden.plants) - garden.GardenStats.plant_added}, "
+                f"Total growth: {garden.GardenStats.growth}cm")
 
     @staticmethod
     def growth_valid(days: int) -> bool:
@@ -63,12 +88,10 @@ class GardenManager:
                 self.plants[i].plant_height += days
                 print(f"{self.plants[i].plant_name} grew {days}cm")
                 i += 1
-            GardenManager.GardenStats(self, len(self.plants) * days)
+            GardenManager.GardenStats.growth = len(self.plants) * days
         else:
             print("You entered an invalid growth parameter")
 
-
-def 
 
 if __name__ == "__main__":
     print("=== Garden Management System Demo ===\n")
@@ -76,5 +99,4 @@ if __name__ == "__main__":
     print("")
     alice.grow(1)
     print("")
-    
     alice.GardenStats.print_stats(alice)
