cff3799b571dff993f34643a863c8716f3bed1fe
llourens <llourens@f0r3s7.codam.nl>
2026-01-08 22:33:04 +0100

    📝 Doc strings

--- Python Modules/Module_02/ex0/ft_first_exception.py
+++ Python Modules/Module_02/ex0/ft_first_exception.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 
 def test_temperature_input() -> None:
+    """
+    Test function for the check_temperature function
+    """
     print("=== Garden Temperature Checker ===\n")
 
-    test_temp = "25"
+    test_temp: str = "25"
     print(f"Testing temperature: {test_temp}")
     check_temperature(test_temp)
 
-    test_temp = "abc"
+    test_temp: str = "abc"
     print(f"Testing temperature: {test_temp}")
     check_temperature(test_temp)
 
-    test_temp = "100"
+    test_temp: str = "100"
     print(f"Testing temperature: {test_temp}")
     check_temperature(test_temp)
 
-    test_temp = "-50"
+    test_temp: str = "-50"
     print(f"Testing temperature: {test_temp}")
     check_temperature(test_temp)
 
 
-def check_temperature(temp_str) -> int:
+def check_temperature(temp_str: str) -> int:
+    """
+    Check's the paraemeter string to see if it is a valid number
+    and within the parameters for a healthy plant.
+
+    Args:
+        temp_str (str): The temperature in string form
+
+    Returns:
+        int: The valid temperature or None
+    """
     try:
         temp: int = int(temp_str)
     except ValueError:
