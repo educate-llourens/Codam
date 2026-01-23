a2a465bf1dd73fc1feaab261999abc47882f2c0e
Leandra Lourens <llourens.codam@gmail.com>
2026-01-23 09:36:27 +0100

    🚧 Ex6: Added ability to add plants

--- Python Modules/Module_01/ex6/ft_garden_analytics.py
+++ Python Modules/Module_01/ex6/ft_garden_analytics.py
@@ -4,7 +4,7 @@
 class Plant:
     """Class containing the name, height and age of the plant
     """
-    def __init__(self, name: str, height: int, age: int):
+    def __init__(self, name: str, height: int):
         """Creates an instance of the Plant class (a plant).
 
         Args:
@@ -14,7 +14,6 @@ class Plant:
         """
         self.name: str = name
         self.height: int = height
-        self.age: int = age
 
 
 class Flower(Plant):
@@ -23,7 +22,7 @@ class Flower(Plant):
     Args:
         Plant (Parent): The parent class
     """
-    def __init__(self, name: str, height: int, age: int,
+    def __init__(self, name: str, height: int,
                  colour: str, bloom: bool):
         """Creates an instance from the Flower class (a flower).
 
@@ -34,15 +33,15 @@ class Flower(Plant):
             colour (str): Colour of the Flower when it blooms
             bloom (bool): If it is blooming or not
         """
-        super().__init__(name, height, age)
+        super().__init__(name, height)
         self.colour: str = colour
         self.bloom: bool = bloom
 
 
 class PrizeFlower(Flower):
-    def __init__(self, name: str, height: int, age: int,
+    def __init__(self, name: str, height: int,
                  colour: str, bloom: bool, points: int):
-        super().__init__(name, height, age, colour, bloom)
+        super().__init__(name, height, colour, bloom)
         self.points: int = points
 
 
@@ -50,30 +49,48 @@ class GardenManager:
     def __init__(self, name: str):
         self.name: str = name
         self.gardens: list[GardenManager.Garden] = []
-
+        self.manager_stats: GardenManager.GardenStats = self.GardenStats(self)
 
     class Garden:
-        def __init__(self, garden_name: str):
+        def __init__(self, garden_name: str, manager: 'GardenManager'):
             self.garden_name: str = garden_name
+            self.manager = manager
             self.plants_list: list[Plant] = []
 
         def add_plants(self, plant: Plant) -> None:
-            self.plants_list.append(plant)
-
+            self.plant = plant
+            self.plants_list.append(self.plant)
+            self.manager.manager_stats.plants_added += 1
+            print(f"Added {self.plant.name} to "
+                  f"{self.garden_name}")
 
     class GardenStats:
-        def __init__(self):
-            self.
+        def __init__(self, manager: 'GardenManager'):
+            self.manager = manager
+            self.plants_added = 0
 
     @classmethod
-    def create_garden_network(cls) -> list['GardenManager']:
+    def create_garden_network(cls) -> 'GardenManager':
         manager = cls("Manager")
-        alice = manager.Garden("Alice's Garden")
+        alice = manager.Garden("Alice's garden", manager)
         manager.gardens.append(alice)
-        bob = manager.Garden("Bob's Garden")
-        manager.gardens.append(alice)
+        bob = manager.Garden("Bob's garden", manager)
+        manager.gardens.append(bob)
         return manager
 
 
 if __name__ == "__main__":
-    print("=== Garden Management System Demo ===")
+    print("=== Garden Management System Demo ===\n")
+
+    # Plants
+    oak_tree = Plant("Oak Tree", 100)
+    rose = Flower("Rose", 25, "red", True)
+    sunflower = PrizeFlower("Sunflower", 50, "yellow", True, 10)
+
+    # Add plants
+    nursery = GardenManager.create_garden_network()
+    nursery.gardens[0].add_plants(oak_tree)
+    nursery.gardens[0].add_plants(rose)
+    nursery.gardens[0].add_plants(sunflower)
+
+    # Grow plants
